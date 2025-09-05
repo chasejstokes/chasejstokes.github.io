65cf40c75fc5560214367c296aebf51efb7f2f4b
chasejstokes <stokeschasej@gmail.com>
2025-09-04 20:37:15 -0500

    testing book dashboard

---
+++ books/js/book_viz.js
@@ -0,0 +1,10 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+
+// preamble function
+export function preamble() {
+
+    const dashboard = d3.select('#dashboard')
+    console.log(dashboard)
+
+}
